18eb8ab099281b21c57816fd7d7131686413a814
Rajdeep.Debnath <Rajdeep.Debnath@RVSKCH269LT>
2018-06-18 15:27:19 +0530

    created the table

--- src/app/new-cmp/new-cmp.component.ts
+++ src/app/new-cmp/new-cmp.component.ts
@@ -19,7 +19,17 @@ export class NewCmpComponent implements OnInit {
 
   ngOnInit() {
     this.api.getAll();
-    this.patientList = [{"Id":"1","Name":"Abcd","Country":"India"},{"Id":"2","Name":"Xyz","Country":"Germany"}];
+    this.patientList = [{"Id":"1","Name":"Aaaa","Country":"India"},
+      {"Id":"2","Name":"Bbb","Country":"Germany"},{"Id":"3","Name":"Ccc","Country":"Germany"},
+      {"Id":"4","Name":"Ddd","Country":"Germany"},{"Id":"5","Name":"Eee","Country":"Germany"},
+      {"Id":"6","Name":"Fff","Country":"Germany"},{"Id":"7","Name":"Ggg","Country":"Germany"},
+      {"Id":"8","Name":"Hhh","Country":"Germany"},{"Id":"9","Name":"Iii","Country":"Germany"},
+      {"Id":"10","Name":"Jjj","Country":"Germany"},{"Id":"11","Name":"Kkk","Country":"Germany"},
+      {"Id":"12","Name":"Lll","Country":"Germany"},{"Id":"13","Name":"Mmm","Country":"Germany"},
+      {"Id":"14","Name":"Nnn","Country":"Germany"},{"Id":"15","Name":"Ooo","Country":"Germany"},
+      {"Id":"16","Name":"Ppp","Country":"Germany"},{"Id":"17","Name":"Qqq","Country":"Germany"},
+      {"Id":"18","Name":"Rrr","Country":"Germany"},{"Id":"19","Name":"Sss","Country":"Germany"},
+      {"Id":"20","Name":"Ttt","Country":"Germany"},{"Id":"21","Name":"Uuu","Country":"Germany"},];
     this.selectedPatient = {"Id":"0","Name":"","Country":""};
   }
 
